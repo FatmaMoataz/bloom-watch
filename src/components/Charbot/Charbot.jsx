1a536e87ccb238e26a47e7ce77d2860ebc92f9da
FatmaMoataz <fatmamoataz65@gmail.com>
2025-09-27 12:58:33 +0200

    chatbot design

--- src/components/Charbot/Charbot.jsx
+++ src/components/Charbot/Charbot.jsx
@@ -4,8 +4,9 @@ export default function Charbot() {
   return (
     <div className="group w-32 opacity-60 hover:opacity-100 cursor-pointer fixed bottom-0 right-0">
       <img src={charbot} alt="Chatbot" />
-      <div className="absolute bottom-full right-0 mb-2 px-3 py-2 bg-gray-800 text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
-        Need help? Click me!
+      <div className="absolute bottom-full right-2 mb-2 px-3 py-2 bg-white text-sm font-semibold text-[#3E2723] rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+        <p>Hi! I'm <span className='text-[#E2758B]'>Bloombot</span> your flower buddy</p>
+        <p>What's on your mind...?</p>       
       </div>
     </div>
   )
